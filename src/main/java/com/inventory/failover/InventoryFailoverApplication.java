c343b6b6955242968e86ca58c5edffe2f651979f
samarth.shah <samarth.shah@einfochips.com>
2022-12-29 14:12:14 +0530

    inventory-failover

---
+++ src/main/java/com/inventory/failover/InventoryFailoverApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.failover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryFailoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryFailoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventory/failover/InventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.failover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryFailoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
